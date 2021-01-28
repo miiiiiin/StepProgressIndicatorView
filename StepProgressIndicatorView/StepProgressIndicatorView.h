1cb06b8dc6b387c43a780f02be45d3b898c60df1
songkyung <min.songkyung@gmail.com>
2021-01-28 16:31:58 +0900

    add carthage

---
+++ StepProgressIndicatorView/StepProgressIndicatorView.h
@@ -0,0 +1,18 @@
+//
+//  StepProgressIndicatorView.h
+//  StepProgressIndicatorView
+//
+//  Created by Running Raccoon on 2021/01/28.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StepProgressIndicatorView.
+FOUNDATION_EXPORT double StepProgressIndicatorViewVersionNumber;
+
+//! Project version string for StepProgressIndicatorView.
+FOUNDATION_EXPORT const unsigned char StepProgressIndicatorViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StepProgressIndicatorView/PublicHeader.h>
+
+
